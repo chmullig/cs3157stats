d8cc134b01fa961496ba5c864577531cbbcbd108
Chris Mulligan <clm2186@columbia.edu>
2013-05-07 23:14:50 -0400

    Basic dump of secret uname, lab number, date.

--- summary.rb
+++ summary.rb
@@ -1,1 +1,19 @@
-#!/usr/bin/env ruby1.9.1
+#!/usr/bin/env ruby
+
+require 'mbox'
+require 'date'
+require 'csv'
+
+unames = Hash.new { |hash, n| hash[n] = (0...8).map{(65+rand(26)).chr}.join }
+CSV.open("commits.csv", "wb") do |csv|
+
+    Dir.glob('*/*.mbox') do |item|
+        name, labn = item.match(/\/([A-z0-9]+)-lab(\d{1,2}).mbox$/).captures
+        puts "working on: #{item}. Lab #{labn} of #{name}. mapped to #{unames[name]}..."
+        Mbox.open(item).each do |mail|
+            #Thu, 18 Apr 2013 22:45:11 -0400
+            d = DateTime.strptime(mail.headers['Date'], "%a, %d %b %Y %T %z")
+            csv << [unames[name], labn, d]
+        end #mail
+    end #glob
+end #csv
