4f1cccca66dc43ea9c5ae84c3fabb8183d8bf880
Chris Mulligan <clm2186@columbia.edu>
2013-05-07 23:47:30 -0400

    dump num files, lines added and removed.

--- summary.rb
+++ summary.rb
@@ -6,14 +6,18 @@ require 'csv'
 
 unames = Hash.new { |hash, n| hash[n] = (0...8).map{(65+rand(26)).chr}.join }
 CSV.open("commits.csv", "wb") do |csv|
+    csv << ['name', 'lab', 'rawdate', 'datetime', 'files', 'insertions', 'deletions']
 
     Dir.glob('*/*.mbox') do |item|
         name, labn = item.match(/\/([A-z0-9]+)-lab(\d{1,2}).mbox$/).captures
-        puts "working on: #{item}. Lab #{labn} of #{name}. mapped to #{unames[name]}..."
+        puts "working on: #{item}. Lab #{labn} of #{name}. mapped to #{unames[name]}...  "
         Mbox.open(item).each do |mail|
             #Thu, 18 Apr 2013 22:45:11 -0400
             d = DateTime.strptime(mail.headers['Date'], "%a, %d %b %Y %T %z")
-            csv << [unames[name], labn, d]
+            if match = mail.content[0].to_str.match(/^ (\d+) files? changed(?:, )?(?:(\d+) insertions?\(\+\))?(?:, )?(?:(\d+) deletions?\(-\))?$/)
+                files, insertions, deletions = match.captures
+            end
+            csv << [unames[name], labn, mail.headers['Date'], d, files || 0, insertions || 0, deletions || 0]
         end #mail
     end #glob
 end #csv
